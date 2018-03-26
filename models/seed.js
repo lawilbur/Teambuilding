82aad0759552213e9abdf2eee046ffbb95adea2e
Alec Wilbur <lawilbur2@gmail.com>
2018-03-26 09:33:42 -0400

    adding final css

--- models/seed.js
+++ models/seed.js
@@ -4,28 +4,28 @@ let seedPosts = [
         time: '10',
         tags: ['Name Game' , 'Icebreaker'],
         needed: ['Tennis Ball'],
-        game: ['Stand participants in a circle facing each other and have them toss a ball to a single person, Before they toss it to the desired person they must say here you go... Mike.. and the person that recieves the ball must reply thank you ... matt, and so on.' , 'After the first round you can add multiple balls to increase the energy level']
+        game: ['Stand participants in a circle facing each other and have them toss a ball to a single person, Before they toss it to the desired person they must say here you go... Mike.. and the person that recieves the ball must reply thank you ... matt, and so on. After the first round you can add multiple balls to increase the energy level']
     },
     { name: 'Have You Ever',
         size: '12+',
         time: '5-10',
         tags: ['Get to Know You' , 'Icebreaker' , 'Energizer'],
         needed: ['Nothing Needed'],
-        game: ['One person stands in middle of circle. They say "Have you ever..." and anybody that has done that thing must move to a new place in the circle. The last person is now in the center and must come up with something else.' , 'Its good to start with the facilitator as an example. "Have you ever...been to New York City?" Everyone that has would need to run to a new spot.']
+        game: ['One person stands in middle of circle. They say "Have you ever..." and anybody that has done that thing must move to a new place in the circle. The last person is now in the center and must come up with something else. Its good to start with the facilitator as an example. "Have you ever...been to New York City?" Everyone that has would need to run to a new spot.']
     },
     { name: 'Battle Bots',
         size: '10-20',
         time: '20+',
         tags: ['Energizer' , 'Initiative'],
         needed: ['Blindfolds' , 'Throwable Objects', 'Rope for Boundry'],
-        game: ["Setup a boundaried area large enough to accomodate serverl people crawling around at a time. Inside the boundry scatter soft throwable objects. Get the group into partners, one blindfolded and one not. Spread the groups out around the outside of the boundry before anyone is blindfolded. The blindfolded partner gets in the boundary on their hands and knees, and must try to throw the objects at the other people in the boundary. Their partner will verbally assist them at finding the objects and knowing where to throw them. Any partner getting hit by an object is out." , 'The facilitator should be monitoring the poeple in the boundry to make sure no one get hurt.']
+        game: ["Setup a boundaried area large enough to accomodate serverl people crawling around at a time. Inside the boundry scatter soft throwable objects. Get the group into partners, one blindfolded and one not. Spread the groups out around the outside of the boundry before anyone is blindfolded. The blindfolded partner gets in the boundary on their hands and knees, and must try to throw the objects at the other people in the boundary. Their partner will verbally assist them at finding the objects and knowing where to throw them. Any partner getting hit by an object is out. The facilitator should be monitoring the poeple in the boundry to make sure no one get hurt."]
     },
     { name: 'Hoola Hoop Pass',
         size: 'Any',
         time: '10+',
         tags: ['Icebreaker' , 'Initiative'],
         needed: ['Hoola Hoop'],
-        game: ["Stand the participants in a circle and have them hold hands. Each person must pass through the hoola hoop until it's made it all the way around the circle. Start over if someone disconnects holding hands." , 'After the first round you can interduce a second hoop going in the opposite direction to make it harder.']
+        game: ["Stand the participants in a circle and have them hold hands. Each person must pass through the hoola hoop until it's made it all the way around the circle. Start over if someone disconnects holding hands. After the first round you can interduce a second hoop going in the opposite direction to make it harder."]
     }
 ]
 
